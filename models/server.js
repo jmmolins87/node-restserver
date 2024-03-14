830d45fef6b7935ef5d04bd0f944c624db203a54
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-03-14 14:22:52 +0100

    wip api

---
+++ models/server.js
@@ -0,0 +1,55 @@
+
+const express = require( 'express' )
+
+class Server {
+    
+    constructor() {
+        
+        this.app = express();
+        this.port = process.env.PORT;
+        
+        // Middlwares
+        this.middlwares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    middlwares() {
+
+        // Directorio público
+        this.app.use( express.static( 'public' ));
+    }
+
+    routes() {
+
+        this.app.get('/api', ( req, res ) => {
+            res.json({ msg: 'get API' })
+        })
+
+        this.app.put('/api', ( req, res ) => {
+            res.json({ msg: 'put API' })
+        })
+
+        this.app.post('/api', ( req, res ) => {
+            res.json({ msg: 'post API' })
+        })
+
+        this.app.delete('/api', ( req, res ) => {
+            res.json({ msg: 'delete API' })
+        })
+
+        this.app.patch('/api', ( req, res ) => {
+            res.json({ msg: 'patch API' })
+        })
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log( `Server is running on port ${ this.port }`);
+        });
+    }
+}
+
+
+module.exports = Server;
